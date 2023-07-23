ec09191a0dc68d259d1eaca84bb1c01d7b790f05
Kartikay Garg <kartikaygarg282@gmail.com>
2023-07-23 16:14:20 +0530

    logger and exception commit

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -11,7 +11,7 @@ class MongoDBClient:
         try:
 
             if MongoDBClient.client is None:
-                mongo_db_url = os.getenv(MONGODB_URL_KEY)
+                mongo_db_url = os.getenv("mongodb+srv://kartikay_garg:kartikay123@cluster0.bn7gtkm.mongodb.net/?retryWrites=true&w=majority")
                 print(mongo_db_url)
                 if "localhost" in mongo_db_url:
                     MongoDBClient.client = pymongo.MongoClient(mongo_db_url) 
