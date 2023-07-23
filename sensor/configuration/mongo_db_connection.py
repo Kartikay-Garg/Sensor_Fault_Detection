05cd9353ef111c1c9411f6d2e9b3257a5da6920d
AyushSingh096 <ayushsingh00112233@gmail.com>
2023-07-24 01:06:19 +0530

    mongodb connection check

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -3,22 +3,20 @@ from sensor.constant.database import DATABASE_NAME
 from sensor.constant.env_variable import MONGODB_URL_KEY
 import certifi
 import os
+
 ca = certifi.where()
 
 class MongoDBClient:
     client = None
-    def __init__(self, database_name=DATABASE_NAME) -> None:
+    def __init__(self, database_name = DATABASE_NAME) -> None:
         try:
-
             if MongoDBClient.client is None:
-                mongo_db_url = os.getenv("mongodb+srv://kartikay_garg:kartikay123@cluster0.bn7gtkm.mongodb.net/?retryWrites=true&w=majority")
-                print(mongo_db_url)
-                if "localhost" in mongo_db_url:
-                    MongoDBClient.client = pymongo.MongoClient(mongo_db_url) 
-                else:
-                    MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
+                
+                #mongo_db_url = os.getenv(MONGODB_URL_KEY)
+                mongo_db_url = "## Paste your mongodb url here##"
+                MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
             self.client = MongoDBClient.client
             self.database = self.client[database_name]
             self.database_name = database_name
         except Exception as e:
-            raise e
+            raise e 
